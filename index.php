5eaa1d2af56539d747c3e5e4059a30e0acf80304
Maysanira <indahermerah@gmail.com>
2022-11-14 19:26:03 +0700

    menambahkan metode set dan get

--- index.php
+++ index.php
@@ -10,15 +10,28 @@ class robot{
     public $berat = 30;
 
 // metode
-public function bersuara(){
-    echo 'suara robotnya ...'.$this->suara;
+// public function bersuara(){
+//     echo 'suara robotnya ...'.$this->suara;
+// }
+// public function berat_robot(){
+//     return $this->berat;
+// }
+
+// metode set (setting) && get
+public function set_suara($suara){
+        $this->suara = $suara;
 }
-public function berat_robot(){
-    return $this->berat;
+public function get_suara(){
+    return $this->suara;
 }
+
 }
 $robot1 = new robot;
-$robot1->bersuara();
-echo $robot1->berat_robot();
+$robot2 = new robot;
+
+$robot1->set_suara('auuu auuu auuuuuuu');
+    echo 'bunyinya ...'.$robot1->get_suara();
+    $robot2->set_suara('tut tut tuttttttt');
+    echo 'bunyinya ...'.$robot2->get_suara();
 // echo 'suara robotnya ...'.$robot1->suara.'beratnya'.$robot1->berat;
 ?>
